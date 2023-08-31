bcd57b3d611b8bd5cf5424d96306cb8d8a47978f
Mohammaderfan Ghasemi <erfann.gh1379@gmail.com>
2023-08-31 13:47:50 +0330

    add os type check in utils

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10
+import platform
 
 import warnings
 
@@ -20,13 +21,30 @@ def load_data():
         ]
     )
 
-    trainset = CIFAR10("./dataset", train=True, download=True, transform=transform)
-    testset = CIFAR10("./dataset", train=False, download=True, transform=transform)
+    # Get the system's platform information
+    system_platform = platform.system()
 
-    num_examples = {"trainset": len(trainset), "testset": len(testset)}
-    return trainset, testset, num_examples
+    # Check if it's Ubuntu
+    if system_platform == "Linux":
+        # Further check if it's Ubuntu
+        distribution = platform.linux_distribution()
+        if "Ubuntu" in distribution:
+            print("Ubuntu detected.")
+        else:
+            print("Linux distribution other than Ubuntu detected.")
+    elif system_platform == "Windows":
+        print("Windows detected.")
+    else:
+        print("Unsupported operating system detected.")
 
 
+    # trainset = CIFAR10("./dataset", train=True, download=True, transform=transform)
+    # testset = CIFAR10(".\lesions_dataset\FL_Test_Dataset", train=False, download=True, transform=transform)
+
+    # num_examples = {"trainset": len(trainset), "testset": len(testset)}
+    # return trainset, testset, num_examples
+
+load_data()
 def load_partition(idx: int):
     """Load 1/10th of the training and test data to simulate a partition."""
     assert idx in range(10)
@@ -93,6 +111,37 @@ def test(net, testloader, steps: int = None, device: str = "cpu"):
     accuracy = correct / len(testloader.dataset)
     net.to("cpu")  # move model back to CPU
     return loss, accuracy
+
+
+# def replace_classifying_layer(efficientnet_model, num_classes: int = 10):
+#     """Replaces the final layer of the classifier."""
+#     num_features = efficientnet_model.classifier.fc.in_features
+#     efficientnet_model.classifier.fc = torch.nn.Linear(num_features, num_classes)
+
+
+# def load_efficientnet(entrypoint: str = "nvidia_efficientnet_b0", classes: int = None):
+#     """Loads pretrained efficientnet model from torch hub. Replaces final
+#     classifying layer if classes is specified.
+
+#     Args:
+#         entrypoint: EfficientNet model to download.
+#                     For supported entrypoints, please refer
+#                     https://pytorch.org/hub/nvidia_deeplearningexamples_efficientnet/
+#         classes: Number of classes in final classifying layer. Leave as None to get the downloaded
+#                  model untouched.
+#     Returns:
+#         EfficientNet Model
+
+#     Note: One alternative implementation can be found at https://github.com/lukemelas/EfficientNet-PyTorch
+#     """
+#     efficientnet = torch.hub.load(
+#         "NVIDIA/DeepLearningExamples:torchhub", entrypoint, pretrained=True
+#     )
+
+#     if classes is not None:
+#         replace_classifying_layer(efficientnet, classes)
+#     return efficientnet
+
 
 
 def replace_classifying_layer(efficientnet_model, num_classes: int = 10):
