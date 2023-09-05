600844bf77ab72d5bb7a927a17deb43026cee5c4
Mohammaderfan Ghasemi <erfann.gh1379@gmail.com>
2023-09-06 00:30:51 +0330

    param changed

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ warnings.filterwarnings("ignore")
 IMAGE_SIZE = 1024  # The size (in pixels) of the images used in the model.
 CENTER_CROP_SIZE = 700  # The size (in pixels) for center cropping the image
 
-LOCAL_LEARNING_RATE = 0.001
+LOCAL_LEARNING_RATE = 0.01
 MOMENTUM = 0.9
 WEIGHT_DECAY = 1e-4
 
